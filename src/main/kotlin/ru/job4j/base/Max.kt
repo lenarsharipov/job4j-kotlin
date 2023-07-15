8f553ba7a253786a834c52b4493144f5df2a46a1
lenarsharipov <lenarsharipov@gmail.com>
2023-07-15 11:20:31 +0300

    4. Операторы условий if [#189221  [#189221 #423150]]

--- src/main/kotlin/ru/job4j/base/Max.kt
+++ src/main/kotlin/ru/job4j/base/Max.kt
@@ -3,6 +3,5 @@ package ru.job4j.base
 fun max(first: Int, second: Int) = if (first >= second) first else second
 
 fun max(first: Int, second: Int, third: Int): Int {
-    val rsl = max(first, second)
-    return if (rsl >= third) rsl else third
+    return max(max(first, second), third)
 }
